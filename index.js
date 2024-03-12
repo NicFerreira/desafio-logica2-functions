ff31bb5b0b494766b54b3c9f70ffaa8f6c9f7e1b
Nícolas Ferreira <nicolasemanuel.f@gmail.com>
2024-03-12 14:00:03 -0300

    Inserindo os códigos das funções para estabelecer rank de Herói

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Iniciando...")
+let heroName = "Owen Higgins";
+let wins = 96;
+let defeats = 10;
+let balance = balanceCalc(wins, defeats);
+let rank = getRank(balance);
+const message = `O Herói ${ heroName } tem saldo de ${ balance } vitórias, e está no nível ${ rank }.`;
+
+function balanceCalc(win, defeat) {
+    let calc = win - defeat;
+    return calc;
+};
+
+function getRank (wins) {
+    if (wins < 10) {
+        return "Ferro"
+    } else if (wins >= 11 && wins <= 20) {
+        return "Bronze"
+    } else if (wins >= 21 && wins <= 50) {
+        return "Prata"
+    } else if (wins >= 51 && wins <= 80) {
+        return "Ouro"
+    } else if (wins >= 81 && wins <= 90) {
+        return "Diamante"
+    } else if (wins >= 91 && wins <= 100) {
+        return "Lendário"
+    } else {
+        return "Imortal"
+    }
+};
+
+console.log(message);
